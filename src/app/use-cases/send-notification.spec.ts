7998c4db4c2bf2d82b207cd4e699b4c42803e524
rick <ricardoanj.14@gmail.com>
2022-12-17 17:29:17 -0300

    feat: create notification repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,8 +1,14 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
 
+const notificationRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  },
+};
 describe('send notification', () => {
   it('Shold be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
